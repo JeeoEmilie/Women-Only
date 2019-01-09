5c59c4a0c5a7f3f770ab4905fec1419c2807ac31
Emilie <jeeo.emilie@gmail.com>
2019-01-09 15:04:58 +0100

    Footer php

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+</div>
+
+
+<footer class="footer">
+
+<nav class="navbar navbar-expand-sm sticky-bottom">
+    <div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="faq.php" title="Foire aux questions">F.A.Q</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="qui-sommes-nous.php" title="Qui sommes-nous">Qui sommes-nous</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.php" title="Nous écrire">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="mentions-legales.php" title="mentions legales"> Mentions légales et CGU</a>
+        </li>
+    </ul>
+</div>
+</nav>
+
+</footer>
+
+
+<!--<footer>
+    <nav class="navbar navbar-expand-lg  navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        
+</div>
+</nav>
+<nav>
+    <ul>
+        lien bas de pages
+        <li><a href="faq.php" title="Foire aux questions">F.A.Q</a></li>
+        <li><a href="qui-sommes-nous.php" title="Qui sommes-nous">Qui sommes-nous</a></li>
+        <li><a href="contact.php" title="Nous écrire">Contact</a></li>
+        <li><a href="mentions-legales.php" title="mentions legales"> Mentions légales et CGU </a></li>
+    </ul>
+</nav>
+
+<div id="bande_horizontale"></div>
+<span> 2018 &copy; women only </span>
+
+</footer>-->
+
+</body>
+
+</html>
